31360adc7fba7225577c9e6e8dc1916f6f245ef0
Arun Murugan <arunmurugan@sahaj.ai>
2023-02-28 19:27:31 +0530

    add Terraform to provision aws resources

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+     }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_key_pair" "tf" {
+  key_name = "tf"
+}
+
+
+resource "aws_security_group" "allow_only_conn_to_server" {
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+  }
+}
+
+resource "aws_instance" "esop_app_server" {
+  ami = "ami-0e742cca61fb65051"
+  instance_type = "t2.micro"
+  tags = {
+    CreatedBy = "arun"
+  }
+
+  key_name = data.aws_key_pair.tf.key_name
+  security_groups = [ aws_security_group.allow_only_conn_to_server.name ]
+}
+
